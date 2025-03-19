6add87f22fb3e66a39c2319b7e40ecd16daa91cf
Thibault CHEVALLERAUD <tchevalleraud@extremenetworks.com>
2025-03-19 12:51:15 +0100

    Add documentation for Version Module

--- plugins/modules/version.py
+++ plugins/modules/version.py
@@ -1,4 +1,75 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+DOCUMENTATION = r"""
+---
+module: version
+short_description: Retrieves the XIQ-SE system version
+description:
+  - This module fetches the system version of XIQ-SE by querying the GraphQL  API.
+  - It is compatible with ExtremeCloudIQ - Site Engine.
+options:
+  xiqse_protocol:
+    description:
+      - Protocol to use for API communication.
+      - Defaults to `"https"`, but can be set to `"http"` if needed.
+    required: false
+    type: str
+    default: "https"
+    choices: ["http", "https"]
+  xiqse_host:
+    description:
+      - IP address or hostname of XIQ-SE.
+    required: true
+    type: str
+  xiqse_port:
+    description:
+      - Port used for API communication.
+      - Defaults to `8443` for HTTPS.
+    required: false
+    type: int
+    default: 8443
+  xiqse_client:
+    description:
+      - Client ID for authentication.
+    required: true
+    type: str
+  xiqse_secret:
+    description:
+      - Secret for authentication.
+    required: true
+    type: str
+    no_log: true
+  xiqse_verify:
+    description:
+      - Whether to verify the SSL certificate when using HTTPS.
+      - Defaults to `true`, Set to `false` to disable verification (useful for self-signed certificates).
+author:
+  - Thibault Chevalleraud (@tchevalleraud)
+"""
+
+EXAMPLES = r"""
+- name: Retrieve version via API
+  version:
+    xiqse_host: "192.168.1.1"
+    xiqse_client: "RzNxMIxcj7"
+    xiqse_secret: "6758749e-2bf3-4b6b-925f-ba599179b5fe"
+  register: result
+
+- name: Display the version
+  debug:
+    msg: "XIQ-SE version: {{ result.version }}"
+"""
+
+RETURN = r"""
+version:
+  description: Detected device version.
+  returned: always
+  type: str
+  sample: "24.10.12.14"
+"""
+
+
 from ansible.module_utils.basic import AnsibleModule
 from ansible_collections.tchevalleraud.extremenetworks_xiqse.plugins.module_utils.utils import get_auth_token, query_graphql
 
