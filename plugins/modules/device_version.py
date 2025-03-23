8aaa5ec10077c99b100f36fff47be920b307e5a0
Thibault CHEVALLERAUD <tchevalleraud@extremenetworks.com>
2025-03-23 14:31:51 +0100

    New module device_version

--- plugins/modules/device_version.py
+++ plugins/modules/device_version.py
@@ -3,13 +3,12 @@
 
 DOCUMENTATION = r"""
 ---
-module: version
+module: device_version
 author:
   - Thibault Chevalleraud (@tchevalleraud)
-short_description: Retrieves the XIQ-SE system version
+short_description: TODO
 description:
-  - This module fetches the system version of XIQ-SE by querying the GraphQL  API.
-  - It is compatible with ExtremeCloudIQ - Site Engine.
+  - TODO
 extends_documentation_fragment:
   - tchevalleraud.extremenetworks_xiqse.provider
 options:
@@ -20,33 +19,12 @@ options:
     default: 30
 """
 
-EXAMPLES = r"""
-- name: Retrieve version via API
-  tchevalleraud.extremenetworks_xiqse.version:
-    provider:
-        host: "192.168.1.1"
-        client_id: "RzNxMIxcj7"
-        client_secret: "6758749e-2bf3-4b6b-925f-ba599179b5fe"
-  register: result
-
-- name: Display the version
-  debug:
-    msg: "XIQ-SE version: {{ result.version }}"
-"""
-
-RETURN = r"""
-version:
-  description: Detected device version.
-  returned: always
-  type: str
-  sample: "24.10.12.14"
-"""
-
 from ansible.module_utils.basic import AnsibleModule
-from ansible_collections.tchevalleraud.extremenetworks_xiqse.plugins.module_utils.utils import get_auth_token, query_graphql
+from ansible_collections.tchevalleraud.extremenetworks.xiqse.plugins.module_utils.utils import get_auth_token, query_graphql
 
 def run_module():
     module_args = dict(
+        ip_address  = dict(type="str", required=True),
         provider    = dict(type="dict", required=True, options=dict(
             protocol        = dict(type="str", required=False, default="https"),
             host            = dict(type="str", required=True),
@@ -63,6 +41,7 @@ def run_module():
         supports_check_mode=True
     )
 
+    device_ip       = module.params["ip_address"]
     provider        = module.params["provider"]
     xiqse_protocol  = provider["protocol"]
     xiqse_host      = provider["host"]
@@ -73,21 +52,28 @@ def run_module():
     timeout         = module.params["timeout"]
 
     query = """
-        query {
-          administration {
-            serverInfo {
-              version
+        query($deviceIp: String!) {
+          network {
+            device(ip: $deviceIp){
+              firmware
             }
           }
         }
     """
 
+    payload = {
+        "query": query,
+        "variables": {
+            "deviceIp": device_ip
+        }
+    }
+
     try:
         token = get_auth_token(xiqse_host, xiqse_client, xiqse_secret, xiqse_port, xiqse_protocol, xiqse_verify, timeout)
-        result = query_graphql(xiqse_host, token, query, {}, xiqse_port, xiqse_protocol, xiqse_verify, timeout)
+        result = query_graphql(xiqse_host, token, query, payload, xiqse_port, xiqse_protocol, xiqse_verify, timeout)
 
-        version = result.get("data", {}).get("administration", {}).get("serverInfo", {}).get("version", "Unknown")
-        module.exit_json(changed=False, version=version)
+        #version = result.get("data", {}).get("administration", {}).get("serverInfo", {}).get("version", "Unknown")
+        module.exit_json(changed=False, version=result)
     except Exception as e:
         module.fail_json(msg=str(e))
 
