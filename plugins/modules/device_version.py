50436c249eeef5bf94ef2876e568fa472ddb8801
Thibault CHEVALLERAUD <thibault.chevalleraud@gmail.com>
2025-06-09 19:06:42 +0200

    Update documentation

--- plugins/modules/device_version.py
+++ plugins/modules/device_version.py
@@ -11,7 +11,6 @@ description:
   - This module allows the collection of equipment versions via the XIQ-SE GraphQL API.
   - It is compatible with ExtremeCloudIQ - Site Engine.
 extends_documentation_fragment:
-  - tchevalleraud.extremenetworks_xiqse.fragments.NOTES_DUAL
   - tchevalleraud.extremenetworks_xiqse.fragments.OPTIONS_IPADDRESS
   - tchevalleraud.extremenetworks_xiqse.fragments.OPTIONS_PROVIDER
   - tchevalleraud.extremenetworks_xiqse.fragments.OPTIONS_TIMEOUT
